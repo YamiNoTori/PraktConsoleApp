0225743fe3f878843d0a1c46430317bc738c9cba
Dmitriy <YamiNoTori>
2022-03-15 13:41:25 +0300

    init project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyLib.Models;
+using MyLib.Controllers;
+
+namespace MyConsoleApp
+{
+
+
+    // Графический юзер интерфейс (GUI)
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+            
+            // debug
+            Console.WriteLine("Приветствую! Тут реализован графический интерфейс компании <company>!\n");        
+            // при старте инициализируется некий контроллер юзера, позволяющий взаимодействовать с 
+            // сущностями пользователей
+            UsersController userController = new UsersController();
+
+            // имитация отправки введённых данных (валидных) авторизации
+            userController.initUser("IAdmin", "qwerty123");
+            userController.initUser("mylogin", "mypassword");
+            // имитация отправки введённых данных (НЕ валидных) авторизации (ОШИБКА)
+           // userController.initUser("IAdфывфыmin", "qwerty123");
+        }
+    }
+}
